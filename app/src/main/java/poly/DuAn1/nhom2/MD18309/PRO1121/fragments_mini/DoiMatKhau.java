f5b549719c5cc1bf5da3ed2bcd0df4a5b6b1e8b6
DongNg64 <dong642001@gmail.com>
2023-12-01 13:05:36 +0700

    the ve item nhan vien, khach hang voi man hinh them cho ca 2 di _ HoangLam said

--- app/src/main/java/poly/DuAn1/nhom2/MD18309/PRO1121/fragments_mini/DoiMatKhau.java
+++ app/src/main/java/poly/DuAn1/nhom2/MD18309/PRO1121/fragments_mini/DoiMatKhau.java
@@ -72,7 +72,7 @@ public class DoiMatKhau extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_doi_mat_khau, container, false);
+        View view = inflater.inflate(R.layout.fragment_doimatkhau, container, false);
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         toolbar.setTitle("");
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
